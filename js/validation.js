99ad8eb46e3e682796d3c02d4be7e35ce9d22341
F3L1P30 <gonzalezfelipe6542@gmail.com>
2023-04-22 23:22:51 -0400

    Validacion Registro
    -Se añaden logos
    -Se añaden comentarios en contador de calorias y imc

--- js/validation.js
+++ js/validation.js
@@ -17,11 +17,31 @@ $("#formulario").validate({
             rangelength: [15, 30],
             
         },
-        input_telefono:{
-            required:true,
-            digits:true,
-            rangelength: [9, 9]
-        }
+        mail: {
+          required: true,
+          email: true
+        },
+        password: {
+          required: true,
+          alphanumeric: true
+        },
+        password2:{
+          required: true,
+          equalTo: "#password"
+        },
+        edad:{
+          required: true,
+          min:1
+        },
+        altura:{
+          required: true,
+          min: 50
+        },
+        peso:{
+          required: true,
+          min: 2
+        },
+
       },
       messages:{
           nombre: {
@@ -34,14 +54,32 @@ $("#formulario").validate({
           },
           usuario: {
             required: "Campo Vacio",
-            rangelength: "Excede el limite de caracteres (30)"
+            rangelength: "Caracteres entre 15 y 30 Porfavor!"
           },
-          input_telefono:{
-              required: "Ingresa un telefono",
-              digits:"Solo Numeros",
-              rangelength: "9 digitos Porfavor"
-
-          }
+          mail: {
+            required: "Campo Vacio",
+            email: "Email Invalido!"
+          },
+          password: {
+            required: "Campo Vacio",
+            
+          },
+          password2:{
+            required: "Campo Vacio",
+            equalTo: "Las contraseñas no son iguales"
+          },
+          edad:{
+            required: "Campo Vacio",
+            min: "Ingrese una edad valida!"
+          },
+          altura:{
+            required: "Campo Vacio",
+            min: "Ingrese una altura valida!"
+          },
+          peso:{
+            required: "Campo Vacio",
+            min: "Ingrese un peso en KG valido!"
+          },
       },
 
     submitHandler: function(form) {
@@ -51,9 +89,13 @@ $("#formulario").validate({
    });
 
    jQuery.validator.addMethod("letras", function(value, element) {
-    return /[a-z," ",A-Z]/.test(value);
+    return /^[a-zA-Z\'\-\sáéíóúñÑÁÉÍÓÚüÜ]+$/.test(value);
   }, "Solo letras");
 
    jQuery.validator.addMethod("espacios", function(value, element) {
     return /[" "]/.test(value);
   }, "Sin espacios");
+
+  jQuery.validator.addMethod("alphanumeric", function(value, element) {
+    return /^\w+$/i.test(value);
+}, "Letras, numeros, y simbolos solamente");
