023ad1af07e6c23dac8792b5361b3850d1feb943
UnaCuentaFalsaMas <70608264+UnaCuentaFalsaMas@users.noreply.github.com>
2023-06-11 14:48:00 -0400

    se cambia 200 a 201

--- src/paginas/Registro.tsx
+++ src/paginas/Registro.tsx
@@ -42,7 +42,7 @@ function Registro() {
           },
         })
         .then((response) => {
-          if (response.status === 200) {
+          if (response.status === 201) {
             setValidated(true);
             console.log('Registro guardado exitosamente');
             form.reset();
