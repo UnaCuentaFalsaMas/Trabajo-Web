95d1ba6f504696c797559813b06f167a4457a2e6
UnaCuentaFalsaMas <70608264+UnaCuentaFalsaMas@users.noreply.github.com>
2023-06-26 20:40:52 -0400

    sdf
    sdf

--- servervice/backend/app.ts
+++ servervice/backend/app.ts
@@ -1,97 +1,70 @@
-import { encriptar } from "./encriptacion";
-import express from "express";
-import mysql from "mysql";
-import requestIp from "request-ip";
+import { encriptar } from './encriptacion';
 
+const express = require('express');
+const mysql = require('mysql');
+const iprequest = require('request-ip');
 const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
 
+//cors
+const cors = require('cors');
+const bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 app.use(cors());
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: 'localhost',
+  user: 'root',
+  password: '',
   port: 3306,
-  database: "LaWeb",
+  database: 'LaWeb',
 });
 
-connection.connect(function (err) {
+connection.connect(function (err: any) {
   if (err) {
-    console.error("Error conectando a la DB " + err.stack);
+    console.error('Error conectando a la DB ' + err.stack);
     return;
   }
-  console.log("Conexión establecida" + connection.threadId);
+  console.log('Conexión establecida' + connection.threadId);
 });
 
-app.post("/registro", jsonParser, (req, res) => {
-  let email = req.body.email;
-  let password = req.body.password;
-  let nombre = req.body.nombre;
-  let apellido = req.body.apellido;
-  let genero = req.body.genero;
-  let edad = req.body.edad;
-  let altura = req.body.altura;
-  let peso = req.body.peso;
-  let llave = req.body.llave;
+app.post('/registro', jsonParser, (req: any, res: any) => {
+  let email = req.body.email; // Obtener el valor del campo 'email' del cuerpo de la solicitud
+  let password = req.body.password; // Obtener el valor del campo 'password' del cuerpo de la solicitud
+  let nombre = req.body.nombre; // Obtener el valor del campo 'nombre' del cuerpo de la solicitud
+  let apellido = req.body.apellido; // Obtener el valor del campo 'apellido' del cuerpo de la solicitud
+  let genero = req.body.genero; // Obtener el valor del campo 'genero' del cuerpo de la solicitud
+  let edad = req.body.edad; // Obtener el valor del campo 'edad' del cuerpo de la solicitud
+  let altura = req.body.altura; // Obtener el valor del campo 'altura' del cuerpo de la solicitud
+  let peso = req.body.peso; // Obtener el valor del campo 'peso' del cuerpo de la solicitud
+  let llave = req.body.llave; // Obtener el valor del campo 'llave' del cuerpo de la solicitud
 
+  // Realizar una consulta a la base de datos para verificar si existe un usuario con el email y password proporcionados
   connection.query(
-    "INSERT INTO usario (email, contrasenia, llave) VALUES (?, ?, ?)",
+    'select * from usuarios where email=? and contrasenia=? and llave=?',
     [email, password, llave],
-    function (error, results) {
+    function (error: any, results: any, fields: any) {
       if (error) throw error;
-
       else {
         connection.query(
-          "INSERT INTO DatosUsuario (select * from usuario where email = ?), nombre, apellido, genero, edad, altura, peso",
-          [email],
-          function (error, results) {
+          "INSERT INTO id_usuarios (id_usuario, nombre, apellido, genero, edad, altura, peso) VALUES (SELECT id_usuario FROM usuario WHERE email = '?', ?, ?, ?, ?, ?, ?)",
+          [email, nombre, apellido, genero, edad, altura, peso],
+          function (error: any, results: any) {
             if (error) throw error;
-            if (results[0]) {
-              res.send(
-                JSON.stringify({
-                  mensaje: false,
-                  resultado: "El email ya existe",
-                })
-              );
-            } else {
-              connection.query(
-                "INSERT INTO usuarios (email, contrasenia, nombre, apellido, genero, edad, altura, peso, llave) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                [
-                  email,
-                  password,
-                  nombre,
-                  apellido,
-                  genero,
-                  edad,
-                  altura,
-                  peso,
-                  llave,
-                ],
-                function (error, results) {
-                  if (error) throw error;
-                  res.send(
-                    JSON.stringify({ mensaje: true, resultado: results })
-                  );
-                }
-              );
-            }
+            res.send(JSON.stringify({ mensaje: true, resultado: results }));
           }
         );
       }
-
       if (results[0] && results[0].activo == 1) {
-        let ip = requestIp.getClientIp(req);
-        let fecha = new Date().toISOString().slice(0, 19).replace("T", " ");
-        let id_usuario = results[0].id;
+        // Si el usuario se encuentra activo, entonces se guardara en la tabla log, la fecha y hora que accedio a la tarea “inicio de sesión”.
 
-        app.post("/log", jsonParser, (req, res) => {
+        let ip = iprequest.getClientIp(req); // Obtener la dirección IP del cliente
+        let fecha = new Date().toISOString().slice(0, 19).replace('T', ' '); // Obtener la fecha y hora actual en formato ISO
+        let id_usuario = results[0].id; // Obtener el id del usuario que inicio sesión
+        app.post('/log', jsonParser, (req: any, res: any) => {
           connection.query(
-            "INSERT INTO log (id_usuario, ip, Tarea, fecha) VALUES (?, ?, ?, ?)",
-            [id_usuario, ip, "inicio de sesión", fecha],
-            function (error, results) {
+            'insert into log (id_usuario, ip, Tarea, fecha) values (?, ?, ?, ?)',
+            [id_usuario, ip, 'inicio de sesión', fecha],
+            function (error: any, results: any, fields: any) {
               if (error) throw error;
             }
           );
@@ -100,18 +73,16 @@ app.post("/registro", jsonParser, (req, res) => {
         // Hacer algo si el usuario no está activo
       }
 
-      res.send(JSON.stringify({ mensaje: true, resultado: results }));
+      res.send(JSON.stringify({ mensaje: true, resultado: results })); // Enviar una respuesta en formato JSON
     }
   );
 });
 
 const configuracion = {
-  hostname: "127.0.0.1",
+  hostname: '127.0.0.1',
   port: 5001,
 };
 
 app.listen(configuracion.port, () => {
-  console.log(
-    `Conectando al servidor http://localhost:${configuracion.port}`
-  );
+  console.log(`Conectando al servidor http://localhost:${configuracion.port}`);
 });
