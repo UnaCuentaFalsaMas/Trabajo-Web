b3f71880b1ff0c0c7b49e309b4eb35d3d655dd3a
UnaCuentaFalsaMas <70608264+UnaCuentaFalsaMas@users.noreply.github.com>
2023-06-26 21:08:15 -0400

    Arreglos api
    se borra codigo inecesario y se arregla nombre base de datos

--- servervice/backend/app.ts
+++ servervice/backend/app.ts
@@ -27,14 +27,14 @@ connection.connect(function (err: any) {
   console.log('Conexión establecida' + connection.threadId);
 });
 
-app.post('/leer', jsonParser, (req: any, res: any) => {
+app.get('/leer', jsonParser, (req: any, res: any) => {
     connection.query('select * from usuarios', function (error: any, results: any, fields: any) {
         if (error) throw error;
         res.send(JSON.stringify({ mensaje: true, resultado: results }));
     });
 });
 
-app.post('/registro', jsonParser, (req: any, res: any) => {
+app.put('/registro', jsonParser, (req: any, res: any) => {
   let email = req.body.email; // Obtener el valor del campo 'email' del cuerpo de la solicitud
   let password = req.body.password; // Obtener el valor del campo 'password' del cuerpo de la solicitud
   let nombre = req.body.nombre; // Obtener el valor del campo 'nombre' del cuerpo de la solicitud
@@ -47,13 +47,13 @@ app.post('/registro', jsonParser, (req: any, res: any) => {
 
   // Realizar una consulta a la base de datos para verificar si existe un usuario con el email y password proporcionados
   connection.query(
-    'insert into usuarios (email, contrasenia, llave) values (?, ?, ?)',
+    'insert into usuario (email, contrasenia, llave) values (?, ?, ?)',
     [email, password, llave],
     function (error: any, results: any, fields: any) {
       if (error) throw error;
       else {
         connection.query(
-          "INSERT INTO id_usuarios (id_usuario, nombre, apellido, genero, edad, altura, peso) VALUES (SELECT id_usuario FROM usuario WHERE email = '?', ?, ?, ?, ?, ?, ?)",
+          "INSERT INTO id_usuario (id_usuario, nombre, apellido, genero, edad, altura, peso) VALUES (SELECT id_usuario FROM usuario WHERE email = '?', ?, ?, ?, ?, ?, ?)",
           [email, nombre, apellido, genero, edad, altura, peso],
           function (error: any, results: any) {
             if (error) throw error;
@@ -61,25 +61,7 @@ app.post('/registro', jsonParser, (req: any, res: any) => {
           }
         );
       }
-      if (results[0] && results[0].activo == 1) {
-        // Si el usuario se encuentra activo, entonces se guardara en la tabla log, la fecha y hora que accedio a la tarea “inicio de sesión”.
-
-        let ip = iprequest.getClientIp(req); // Obtener la dirección IP del cliente
-        let fecha = new Date().toISOString().slice(0, 19).replace('T', ' '); // Obtener la fecha y hora actual en formato ISO
-        let id_usuario = results[0].id; // Obtener el id del usuario que inicio sesión
-        app.post('/log', jsonParser, (req: any, res: any) => {
-          connection.query(
-            'insert into log (id_usuario, ip, Tarea, fecha) values (?, ?, ?, ?)',
-            [id_usuario, ip, 'inicio de sesión', fecha],
-            function (error: any, results: any, fields: any) {
-              if (error) throw error;
-            }
-          );
-        });
-      } else {
-        // Hacer algo si el usuario no está activo
-      }
-
+      
       res.send(JSON.stringify({ mensaje: true, resultado: results })); // Enviar una respuesta en formato JSON
     }
   );
